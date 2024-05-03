6512f7dafeb177330b1fec2f5044311c4c934bc8
JoaquinBaldevenito <joaquinbaldevenito@gmail.com>
2024-05-03 19:58:40 -0300

    Un poco de todo

--- proyecto/SurvivalWisdom.rb
+++ proyecto/SurvivalWisdom.rb
@@ -2,15 +2,22 @@ require 'sinatra'
 set :public_folder, 'assets'  # Assuming your assets are in a folder named 'assets'
 
 get '/login' do
+  puts "Hola"  
   erb :index
 end
+
 post '/login' do
     username = params[:first]
     password = params[:password]
+
+    @user = User.where(username: username).first
     
+    if @user.password == password
+    else
+    end
     # Aquí podrías agregar lógica para verificar las credenciales del usuario
+    erb :home
     
-    "¡Hola, #{username}! Tu contraseña es #{password}."
 end
 
 get '/register' do
