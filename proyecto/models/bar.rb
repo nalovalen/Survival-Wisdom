3481bcbd7d81e9b7bb9723e6d09728f0ed82f7ab
Loser Facundo Dario <loserfacundodario@gmail.com>
2024-05-10 17:18:06 -0300

    añadiendo métodos útiles a la clase bar

--- proyecto/models/bar.rb
+++ proyecto/models/bar.rb
@@ -1,4 +1,27 @@
 class Bar < ActiveRecord::Base
     has_many :test
     has_many :option
+
+    #constructor (valor por defecto es 10 si no se pasa un parámetro)
+    def initialize(value = 10)
+        @value = value
+    end
+
+    #aplica el efecto damage a la barra de salud:
+    def apply_damage(damage)
+      self.value += damage
+      self.save
+    end
+
+    #v2:
+    #esta me asegura que cuando resto no me pase de 0 y la barra sea negativa
+    def apply_damageV2(damage)
+        self.value = [self.value - damage, 0].max
+        self.save
+    end
+
+    #retorna el valor de la barra [0..10]
+    def value
+        @value
+    end
 end
