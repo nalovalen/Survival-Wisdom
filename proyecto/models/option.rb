7a9f8d4c6982ad96d9026503bcd9bdcfb07b1217
Loser Facundo Dario <loserfacundodario@gmail.com>
2024-05-12 13:11:48 -0300

    añadiendo métodos a la clase Option

--- proyecto/models/option.rb
+++ proyecto/models/option.rb
@@ -2,4 +2,21 @@ class Option < ActiveRecord::Base
     has_many :bars
     has_one :answer
     belongs_to :question
+
+    #constructor
+    def initialize(attributes = {})
+      super
+      @description = attributes[:description]
+      @effects = attributes[:effects]
+    end
+
+    #getter de descricpción
+    def description
+      @description
+    end
+
+    #getter de los efectos
+    def effects
+      @effects
+    end
 end
